c7a7ce3fa2d4fd4dc75bae96e84b32f1595e2c61
xudongdong <398713567@qq.com>
2019-06-18 23:39:16 +0800

    侧边栏初步布局以及iconfont引入

--- static/spa/assets/js/assetmanage/list.js
+++ static/spa/assets/js/assetmanage/list.js
@@ -1,9 +1,9 @@
-layui.use(['admin', 'table', 'form', 'dropdown'], function() {
+layui.use(['admin', 'table', 'form', 'dropdown'], function () {
     var table = layui.table,
         form = layui.form,
         $ = layui.$,
         admin = layui.admin,
-    dropdown = layui.dropdown;
+        dropdown = layui.dropdown;
 
     form.render();
 
@@ -11,9 +11,9 @@ layui.use(['admin', 'table', 'form', 'dropdown'], function() {
     $.ajax({
         url: '/assetmanage/assettype/',
         type: 'GET',
-        done: function(res) {
+        done: function (res) {
             let data = res.data;
-            $.each(data, function(index, item) {
+            $.each(data, function (index, item) {
                 $('#assettype').append(new Option(item.name, item.id));
             });
             form.render()
@@ -90,87 +90,92 @@ layui.use(['admin', 'table', 'form', 'dropdown'], function() {
         ],
         page: true
     });
-// 
-// 		//头工具栏事件
-// 		table.on('toolbar(rootasset-list)', function(obj) {
-// 			switch (obj.event) {
-// 				case 'add':
-// 					admin.popup({
-// 						title: '资产录入',
-// 						area: ['500px', '660px'],
-// 						fixed: true,
-// 						maxmin: true,
-// 						success: function(layero, index) {
-// 							//将 views 目录下的某视图文件内容渲染给该面板
-// 							layui.view(this.id).render('assetmanage/create').done(function() {
-// 
-// 							});
-// 						},
-// 						end: function() {
-// 							table.reload('rootasset-list');
-// 						}
-// 					});
-// 			};
-// 		});
-// 
-// 		//监听行工具事件
-// 		table.on('tool(rootasset-list)', function(obj) {
-// 			var data = obj.data;
-// 			if (obj.event === 'del') {
-// 				layer.confirm('确定删除资产', function(index) {
-// 					admin.req({
-// 						url: '/assetmanage/assetdelete/' + data.id + "/" //实际使用请改成服务端真实接口
-// 							,
-// 						type: 'GET',
-// 						done: function(res) {
-// 							//成功的提示与跳转
-// 							layer.msg(res.msg, {
-// 								offset: '15px',
-// 								icon: 1,
-// 								time: 1000
-// 							}, function() {
-// 								layui.index.render();
-// 							});
-// 						}
-// 					});
-// 					obj.del();
-// 					layer.close(index);
-// 				});
-// 			} else if (obj.event === 'detial') {
-// 				location.hash = '/assetmanage/assetdetial/id=' + data.id;
-// 			} else if (obj.event === 'edit') {
-// 				admin.popup({
-// 					title: '修改资产信息',
-// 					area: ['500px', '660px'],
-// 					fixed: true,
-// 					maxmin: true,
-// 					success: function(layero, index) {
-// 						//将 views 目录下的某视图文件内容渲染给该面板
-// 						layui.view(this.id).render('assetmanage/create').done(function() {
-// 							$("#id").val(data.id);
-// 							$("#name").val(uncode(data.name));
-// 							$("#key").val(uncode(data.key));
-// 							$("#type").html(data.type);
-// 							$("#manage").val(uncode(data.manage));
-// 							$("#telephone").val(data.telephone);
-// 							$("#email").val(uncode(data.email));
-// 							$("#description").val(uncode(data.description));
-// 							$("#parent_name").val(uncode(data.parent_check.name)).attr("ts-selected", data.parent_check.id);
-// 							$("#parent").val(data.parent_check.id);
-// 						});
-// 					},
-// 					end: function() {
-// 						table.reload('rootasset-list');
-// 					}
-// 				});
-// 			} else if (obj.event == 'info') {
-// 				layer.tips(data.description, '.layui-table-hover', {
-// 					tips: 1,
-// 					time: 4000
-// 				});
-// 			};
-// 		});
-// 
+    // 
+    // 		//头工具栏事件
+    table.on('toolbar(rootasset-list)', function (obj) {
+        switch (obj.event) {
+            case 'add':
+                admin.open({
+                    title: '资产录入',
+                    area: ['500px', '660px'],
+                    url: './views/assetmanage/assetcreate.html'
+                });
+            // admin.popup({
+            // 	title: '资产录入',
+            // 	area: ['500px', '660px'],
+            // 	fixed: true,
+            // 	maxmin: true,
+            // 	success: function(layero, index) {
+            // 		//将 views 目录下的某视图文件内容渲染给该面板
+            // 		layui.view(this.id).render('assetmanage/create').done(function() {
+
+            // 		});
+            // 	},
+            // 	end: function() {
+            // 		table.reload('rootasset-list');
+            // 	}
+            // });
+        };
+    });
+    // 
+    // 		//监听行工具事件
+    // 		table.on('tool(rootasset-list)', function(obj) {
+    // 			var data = obj.data;
+    // 			if (obj.event === 'del') {
+    // 				layer.confirm('确定删除资产', function(index) {
+    // 					admin.req({
+    // 						url: '/assetmanage/assetdelete/' + data.id + "/" //实际使用请改成服务端真实接口
+    // 							,
+    // 						type: 'GET',
+    // 						done: function(res) {
+    // 							//成功的提示与跳转
+    // 							layer.msg(res.msg, {
+    // 								offset: '15px',
+    // 								icon: 1,
+    // 								time: 1000
+    // 							}, function() {
+    // 								layui.index.render();
+    // 							});
+    // 						}
+    // 					});
+    // 					obj.del();
+    // 					layer.close(index);
+    // 				});
+    // 			} else if (obj.event === 'detial') {
+    // 				location.hash = '/assetmanage/assetdetial/id=' + data.id;
+    // 			} else if (obj.event === 'edit') {
+    // 				admin.popup({
+    // 					title: '修改资产信息',
+    // 					area: ['500px', '660px'],
+    // 					fixed: true,
+    // 					maxmin: true,
+    // 					success: function(layero, index) {
+    // 						//将 views 目录下的某视图文件内容渲染给该面板
+    // 						layui.view(this.id).render('assetmanage/create').done(function() {
+    // 							$("#id").val(data.id);
+    // 							$("#name").val(uncode(data.name));
+    // 							$("#key").val(uncode(data.key));
+    // 							$("#type").html(data.type);
+    // 							$("#manage").val(uncode(data.manage));
+    // 							$("#telephone").val(data.telephone);
+    // 							$("#email").val(uncode(data.email));
+    // 							$("#description").val(uncode(data.description));
+    // 							$("#parent_name").val(uncode(data.parent_check.name)).attr("ts-selected", data.parent_check.id);
+    // 							$("#parent").val(data.parent_check.id);
+    // 						});
+    // 					},
+    // 					end: function() {
+    // 						table.reload('rootasset-list');
+    // 					}
+    // 				});
+    // 			} else if (obj.event == 'info') {
+    // 				layer.tips(data.description, '.layui-table-hover', {
+    // 					tips: 1,
+    // 					time: 4000
+    // 				});
+    // 			};
+    // 		});
+    // 
 
     //查询并重载表格
     form.on('submit(query)', (d) => {
