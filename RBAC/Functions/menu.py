83a411098d74809e69ed35a9d1c131b34ec58b7b
gy071089 <gy071089@outlook.com>
2019-06-19 23:43:35 +0800

    menu update

--- RBAC/Functions/menu.py
+++ RBAC/Functions/menu.py
@@ -29,11 +29,12 @@ def menutotree_easyweb(menu_list,is_root=True):
         menu_node = {
             #'name':escape(menu_get.key)
             'name':escape(menu_get.name)
+            ,'url':escape(menu_get.jump)
             ,'icon':escape(menu_get.icon)
             ,'subMenus':[]
             }
         if menu_get.menu_menu.all():
-            menu_node['subMenus'] = menutotree(menu_get.menu_menu.all(),False)
+            menu_node['subMenus'] = menutotree_easyweb(menu_get.menu_menu.all(),False)
         if not is_root:
             menu_node['url'] = escape(menu_get.jump)
         menu_tree.append(menu_node)
